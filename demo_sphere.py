ef11303757e19a505667013d389ae8b62893a36b
Benjamin Lefaudeux <benjamin.lefaudeux@pm.me>
2022-02-02 21:14:16 -0800

    adding a pure rendering path, checking that it works indeed. Optimization still dies on autodiff

--- demo_sphere.py
+++ demo_sphere.py
@@ -1,11 +1,12 @@
 from scene import Scene
 import taichi as ti
+from typing import Tuple
 
 
 @ti.kernel
 def write_circle(buffer: ti.template(), circle_radius: float):
     center = ti.Vector([buffer.shape[0] / 2.0, buffer.shape[1] / 2.0])
-    sq_radius = circle_radius**2
+    sq_radius = circle_radius ** 2
 
     for u, v in buffer:
         pos = ti.Vector([u, v], dt=ti.float32) - center
@@ -28,7 +29,46 @@ def write_poses(pose: ti.template()):  # type: ignore
     )
 
 
-def demo_sphere(n_views: int = 10):
+@ti.kernel
+def write_sphere(grid: ti.template(), grid_size: int):  # type: ignore
+    span = ti.Vector([grid_size, grid_size, grid_size]) / 4
+    sq_radius = span.norm_sqr()
+
+    for x, y, z in grid:
+        dist = ti.Vector([x, y, z]) - span
+        if dist.norm_sqr() > sq_radius:
+            grid[x, y, z] = 0.0
+        else:
+            grid[x, y, z] = 1.0
+
+
+def demo_sphere_render(n_views: int = 10):
+    # Build the Chinoxel scene
+    n_nodes = 20
+    resolution = (320, 320)
+    focal = 1 / 320.0
+    scene = Scene(
+        grid_size=(n_nodes, n_nodes, n_nodes),
+        resolution=resolution,
+        focal=focal,
+        max_depth_ray=1,
+        LR=0.1,
+    )
+
+    # Write a sphere in the scene, and check the rendering phase
+    write_sphere(scene.grid, n_nodes)
+
+    # Check the rendering
+    gui = ti.GUI("Chinoxel", resolution[0], fast_gui=False)
+    gui.fps_limit = 60
+
+    while True:
+        scene.render()
+        gui.set_image(scene.view_buffer)
+        gui.show()
+
+
+def demo_sphere_optimize(n_views: int = 10):
     # Build the Chinoxel scene
     n_nodes = 20
     resolution = (320, 320)
@@ -62,4 +102,4 @@ def demo_sphere(n_views: int = 10):
 
 
 if __name__ == "__main__":
-    demo_sphere()
+    demo_sphere_render()
